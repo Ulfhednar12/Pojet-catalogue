3168a80afcb5defaac5638a895b9e3db66df0ab0
Djulo988 <julienlaligue.ejl@gmail.com>
2023-05-31 15:34:21 +0200

    test caroussel

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300&display=swap" rel="stylesheet">
+    <!-- Boostrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- Bootstrap 5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     
     
     <title>Catalogue</title>
@@ -39,7 +44,54 @@
     
 </header>
 
+<div id="carouselExampleIndicators" class="carousel" data-bs-ride="true" >
 
+    <!-- Les indicateurs (slide actif) du carousel -->
+    <div class="carousel-indicators" >
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <!-- Les slides du carousel -->
+    <div class="carousel-inner">
+
+        <!-- Slide 1 (slide actif) -->
+        <div class="carousel-item active">
+
+            <!-- La row -->
+            <div class="row" >
+
+                <!-- Les colonnes responsives : N colonnes -->
+                <div class="col-md-3 col-sm-6 col-6" ></div>
+                <div class="col-md-3 col-sm-6 col-6" ></div>
+                <div class="col-md-3 col-sm-6 col-6" ></div>
+                <div class="col-md-3 col-sm-6 col-6" ></div>
+
+            </div>
+
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item"></div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item"></div>
+
+    </div>
+
+    <!-- Les boutons suivant et pécédent -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+
+</div>
 
 <div class="card">
     <h1 class="films_affiche">Films à l'affiche</h1>
